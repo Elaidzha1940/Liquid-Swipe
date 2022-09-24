535dc41228235ff6c60db5f15e61f09da6fbec64
Elaidzha1940 <eli080293@gmail.com>
2022-09-24 06:07:55 +0300

    Initial Commit

---
+++ Liquid S/Liquid_SApp.swift
@@ -0,0 +1,17 @@
+//
+//  Liquid_SApp.swift
+//  Liquid S
+//
+//  Created by Elaidzha Shchukin on 24.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct Liquid_SApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
